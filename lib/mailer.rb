095fab96da72825c746fd69f4c5cd0a289dd582c
Atanas Zlatarev emerchantpay <atanas.zlatarev@emerchantpay.com>
2021-04-28 22:13:11 +0300

    mailer helper to check for reset pass mails and rspec

---
+++ lib/mailer.rb
@@ -0,0 +1,56 @@
+# Checks for lost password mails.
+class Mailer
+  def initialize
+    @gmail       = Gmail.connect(Environment::MAIL[:address], 'Test1234!!')
+    @subject     = Environment::MAIL[:subject]
+    @from        = Environment::MAIL[:from]
+    @max_retries = Environment::MAIL[:retries]
+    @retry_sleep = Environment::MAIL[:retry_sleep]
+  end
+
+  def self.recieved_lost_pass_mail
+    Mailer.new.check_unread_lost_pass
+  end
+
+  def check_unread_lost_pass
+    read_all_lost_pass_mails
+    return false unless retry_recieve_lost_pass_mail
+
+    read_all_lost_pass_mails
+  end
+
+  private
+
+  attr_reader :gmail
+
+  def read_all_lost_pass_mails
+    gmail.inbox.find(:unread,
+                     subject: @subject,
+                     after:   Date.parse(Time.now.utc.strftime('%Y%m%d_%H')),
+                     from:    @from).each do |email|
+      # p email.message.from
+      p email.subject
+      # p email.message.body.raw_source
+      # p email.message.body.raw_source.include?('Reset password')
+      email.read!
+    end
+  end
+
+  def retry_recieve_lost_pass_mail
+    @max_retries.times do
+      unread_count = check_unread_lost_pass_count
+      return true if unread_count.positive?
+
+      sleep(@retry_sleep)
+    end
+
+    false
+  end
+
+  def check_unread_lost_pass_count
+    gmail.inbox.count(:unread,
+                      subject: @subject,
+                      after:   Date.parse(Time.now.utc.strftime('%Y%m%d_%H')),
+                      from:    @from)
+  end
+end
---
+++ spec/ui/account/reset_pass_spec.rb
@@ -0,0 +1,26 @@
+describe 'Reset password', :ui do
+  let(:page) { Browser.open_new_page }
+
+  context 'when filling lost password form' do
+    it 'sends mail', :mail do
+      page.navigate.to Environment::UI[:login_url]
+
+      page.find_element(:link, 'Unable to log in?').click
+
+      user_field = page.find_element(:id, 'admin_user_login')
+      user_field.click
+      user_field.send_keys 'admin'
+
+      user_field = page.find_element(:id, 'admin_user_email')
+      user_field.click
+      user_field.send_keys 'genesis.test.automation@gmail.com'
+
+      expect(page.title).to be == 'Passwords'
+
+      submit_button = page.find_element(:name, 'commit')
+      submit_button.click
+
+      expect(Mailer.recieved_lost_pass_mail).to be true
+    end
+  end
+end
