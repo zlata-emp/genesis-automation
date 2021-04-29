208ad3b658ca06aa9484e838d533391ed9a75cc1
Atanas Zlatarev emerchantpay <atanas.zlatarev@emerchantpay.com>
2021-04-29 12:56:06 +0300

    alternative rspec format example

--- spec/api/consumer/create_consumer_spec.rb
+++ spec/api/consumer/create_consumer_spec.rb
@@ -1,10 +1,8 @@
 describe 'Create consumer', :api, :consumer do
-  let(:request_file) { 'consumer/create_consumer_request.xml' }
-  let(:request) { PostData::Request.build_and_submit(request_file, params) }
-
   context 'with valid non existing email' do
-    let(:valid_email) { "test_email_#{Time.now.utc.strftime('%Y%m%d_%H%M%S')}@emerchantpay.com" }
-    let(:params)      { { email: valid_email } }
+    let(:request_file) { 'consumer/create_consumer_request.xml' }
+    let(:valid_email)  { "test_email_#{Time.now.utc.strftime('%Y%m%d_%H%M%S')}@emerchantpay.com" }
+    let(:request)      { PostData::Request.build_and_submit(request_file, { email: valid_email }) }
 
     it 'returns status error' do
       expect(request.response_body_root).to include({ status: 'enabled' })
@@ -13,17 +11,18 @@ describe 'Create consumer', :api, :consumer do
 
   context 'with erroneous request', :erroneous do
     context 'with existing email' do
-      let(:params) { { email: 'consumer@email.com' } }
-
+      request = PostData::Request.build_and_submit('consumer/create_consumer_request.xml',
+                                                   { email: Environment::CONSUMER[:consumer_email] })
       it 'returns status error' do
         expect(request.response_body_root).to include({ status: 'error' })
       end
     end
 
     context 'without email' do
-      let(:params) { { email: nil } }
-
       it 'returns status error' do
+        request = PostData::Request.build_and_submit('consumer/create_consumer_request.xml',
+                                                     { email: nil })
+
         expect(request.response_body_root).to include({ status: 'error' })
       end
     end
