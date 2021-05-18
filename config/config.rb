b2ceb028c40c74f19e249044832924791a9e54a1
Atanas Zlatarev emerchantpay <atanas.zlatarev@emerchantpay.com>
2021-05-18 16:38:47 +0300

    rspec pattern config for wpf multi subdirs

--- config/config.rb
+++ config/config.rb
@@ -1,18 +1,35 @@
 # frozen_string_literal: true
 
 class Config
-  LOGGER_LEVEL          = Logger::WARN
+  LOGGER_LEVEL          = Logger::DEBUG
   REQUESTS_DIR          = 'requests'
   TRANSACTION_ID_PREFIX = 'qa_checks-'
 
   # This seems like a hack, probably it will be better to just list them in the Environment class
   def self.dirs_containing_spec_files
     dirs_defined_in_env_list = Environment.constants - Config.constants
-    dirs_defined_in_env_list = dirs_defined_in_env_list.each(&:to_s).join(',').downcase
-    return dirs_containing_specs_list unless Environment.const_defined?(:PROCESSING)
+    dirs_containing_specs_list = dirs_defined_in_env_list.each(&:to_s).join(',').downcase
 
-    processing_subdirs = Environment::PROCESSING.keys.each(&:to_s).join(',').downcase
+    dirs_containing_specs_list = add_processing_subdirs(dirs_containing_specs_list)
 
-    "#{dirs_defined_in_env_list}}{,/}{,#{processing_subdirs}"
+    add_wpf_subdirs(dirs_containing_specs_list)
+  end
+
+  def self.add_processing_subdirs(dirs_containing_specs_list)
+    if Environment.const_defined?(:PROCESSING)
+      processing_subdirs = Environment::PROCESSING.keys.each(&:to_s).join(',processing/').downcase
+      dirs_containing_specs_list = dirs_containing_specs_list.gsub 'processing', "processing/#{processing_subdirs}"
+    end
+
+    dirs_containing_specs_list
+  end
+
+  def self.add_wpf_subdirs(dirs_containing_specs_list)
+    if Environment.const_defined?(:WPF)
+      wpf_subdirs = Environment::WPF.keys.each(&:to_s).join(',wpf/').downcase
+      dirs_containing_specs_list = dirs_containing_specs_list.gsub 'wpf', "wpf/#{wpf_subdirs}"
+    end
+
+    dirs_containing_specs_list
   end
 end
